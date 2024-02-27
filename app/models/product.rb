1e2e25dc231c32fe1d2f3619390e31d3eb5e1e26
Francisco Mancilla <frmancil@hotmail.com>
2024-02-27 14:25:33 -0600

    seed executed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,2 @@
 class Product < ApplicationRecord
-    validates title, presence: true
-    validates price, presence: true
-    validates stock_quantity, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+676.times do
+    product = Product.new(title: Faker::Name.first_name, price: Faker::Commerce.price, stock_quantity: Faker::Number.number(digits: 2))
+    product.save
+end
